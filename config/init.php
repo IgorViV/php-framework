e750159993758ec6f9adb33b2baf3da181efc66b
IgorViV <iupgrader@mail.ru>
2023-02-27 13:38:33 +0300

    Performs application configuration

---
+++ config/init.php
@@ -0,0 +1,18 @@
+<?php
+
+define("DEBUG", 1); // 0 production
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/frm');
+define("HELPERS", ROOT . '/vendor/frm/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'main');
+define("PATH", 'http://frm');
+define("ADMIN", 'http://frm/admin');
+define("NO_IMAGE", 'uploads/no_image.jpg');
+
+require_once ROOT . '/vendor/autoload.php';
+
