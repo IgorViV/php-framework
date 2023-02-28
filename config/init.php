9256acd9ecb377af1caa68998fe4e1f13d1f53b6
IgorViV <iupgrader@mail.ru>
2023-02-28 22:56:30 +0300

    Rename project

--- config/init.php
+++ config/init.php
@@ -4,14 +4,14 @@ define("DEBUG", 1); // 0 production
 define("ROOT", dirname(__DIR__));
 define("WWW", ROOT . '/public');
 define("APP", ROOT . '/app');
-define("CORE", ROOT . '/vendor/frm');
-define("HELPERS", ROOT . '/vendor/frm/helpers');
+define("CORE", ROOT . '/vendor/frw');
+define("HELPERS", ROOT . '/vendor/frw/helpers');
 define("CACHE", ROOT . '/tmp/cache');
 define("LOGS", ROOT . '/tmp/logs');
 define("CONFIG", ROOT . '/config');
 define("LAYOUT", 'main');
-define("PATH", 'http://frm');
-define("ADMIN", 'http://frm/admin');
+define("PATH", 'http://frw');
+define("ADMIN", 'http://frw/admin');
 define("NO_IMAGE", 'uploads/no_image.jpg');
 
 require_once ROOT . '/vendor/autoload.php';
